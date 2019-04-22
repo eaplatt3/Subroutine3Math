92279ebb8d44ef6fae45f20f476f42bfcb56a3b8
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-04-22 19:01:46 -0400

    debugging

--- Subroutine3Math.pl
+++ Subroutine3Math.pl
@@ -2,7 +2,7 @@
 # Earl Platt III
 # 4/22/2019
 # BCS 316 - Perl Programming
-# Subroutine 3 Number Math
+# Subroutine 3 Numbers Math
 # Input - User Enters Numbers
 # Output - Math Done with Numbers 
 ####################################
@@ -15,33 +15,27 @@ my $subNum2;
 my $subNum3;
 my $tot;
 
-
-
 sub enterNums{
-	
 	my $num1 = <STDIN>;
 	chomp $num1;
 	my $num2 = <STDIN>;
 	chomp $num2;
 	my $num3 = <STDIN>;
 	chomp $num3;
-	
-	
+	#return $num1, $num2, $num3;
 }
 
 sub multi {
+
 	$tot = $subNum1 * $subNum2 * $subNum3;
 	return $tot;
 }
 
 print "\n";
-print "Please Enter Numbers: "; 
+print "Please Enter 3 Numbers"; 
+print "\n";
 
-enterNums($subNum1, $subNum2, $subNum3);
-
-
-
-multi($subNum1, $subNum2, $subNum3);
+#multi(enterNums());
 
 
 print "$tot"; 
